56541a4bffd7f1b329f16cdefb319c7f09a6dc2a
Rue <viholrutu2000@gmail.com>
2023-10-04 09:01:43 -0400

    Update Continent.cpp

--- Continent/Continent.cpp
+++ Continent/Continent.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-#include <vector>
-#include "Territory.h"
+#include "Continent.h"
 
-class Continent {
-public:
-    Continent(const std::string &name);
-    ~Continent();
-
-    Continent(const Continent &c); // copy constructor
-    Continent& operator=(const Continent &c); // assignment operator
-    friend std::ostream& operator<<(std::ostream &os, const Continent &c); // stream insertion operator
-
-    void addTerritory(Territory *territory);
-    const std::vector<Territory*>& getTerritories() const;
-
-private:
-    std::string *name;
-    std::vector<Territory*> territories;
-};
-
+// Implementations for Continent methods and operators go here. 
+// Remember to manage memory appropriately.
