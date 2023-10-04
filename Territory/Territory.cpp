631fb847b8a7d3f7d6dc82ffcbbac0a116aa14dd
Rue <viholrutu2000@gmail.com>
2023-10-04 09:02:47 -0400

    Update Territory.cpp

--- Territory/Territory.cpp
+++ Territory/Territory.cpp
@@ -1,25 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include "Continent.h"
 #include "Territory.h"
 
-using namespace std;
-
-
-Territory::Territory(int id, const string& name){
-    territory_name = name;
-    this->id = id;
-    continent = nullptr;
-}
-
-
-void Territory::AddNeighbour (Territory * neighbour){ // Adding neighbouring territories
-
-    neighbours.push_back(neighbour);
-}
-
-
-
-
-// Now you can use the Territory class in your Continent class or any other part of your code.
+// Implementations for Territory methods and operators go here. 
+// This includes memory management: proper allocation in constructors and deallocation in destructors.
