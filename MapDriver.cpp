a4183e1745cd11ef1bd9e922a767845715f03d5c
Rue <viholrutu2000@gmail.com>
2023-10-04 09:07:27 -0400

    Update MapDriver.cpp

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,2 +1,23 @@
-#include <iostream>
+#include "Map.h"
+#include "MapLoader.h"
+
+void testLoadMaps() {
+    // Example code: adapt it for multiple files, handle memory cleanup etc.
+    MapLoader loader("path_to_map_file.map");
+    Map *map = loader.loadMap();
+
+    if(map && map->validate()) {
+        std::cout << "Valid map!" << std::endl;
+    } else {
+        std::cout << "Invalid map!" << std::endl;
+    }
+
+    delete map;  // Clean up memory
+}
+
+int main() {
+    testLoadMaps();
+    return 0;
+}
+
 
