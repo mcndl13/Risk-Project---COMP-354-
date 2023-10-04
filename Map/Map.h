300c3c9239188d6cb667b296bdeda850177d4c7a
Rue <viholrutu2000@gmail.com>
2023-10-04 09:04:30 -0400

    Update Map.h

--- Map/Map.h
+++ Map/Map.h
@@ -1,3 +1,26 @@
+#include <iostream>
+#include <vector>
+#include "Continent.h"
+#include "Territory.h"
+
+class Map {
+public:
+    Map();
+    ~Map();
+
+    Map(const Map &m); // copy constructor
+    Map& operator=(const Map &m); // assignment operator
+    friend std::ostream& operator<<(std::ostream &os, const Map &m); // stream insertion operator
+
+    bool validate() const;
+    void addTerritory(Territory *territory);
+    void addContinent(Continent *continent);
+
+private:
+    std::vector<Territory*> territories;
+    std::vector<Continent*> continents;
+};
+
 /*
 
 map rules:
