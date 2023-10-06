5448d9f183858d889c9a95f1282899c5aaef618b
Rue <viholrutu2000@gmail.com>
2023-10-06 17:52:34 -0400

    Update MapDriver.cpp

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,23 +1,65 @@
+// The testLoadMaps function now takes a vector of strings containing the paths to map files.
+//The main function creates a vector of map file paths and passes it to testLoadMaps.
+//The testLoadMaps function iterates through each map file, loading, validating, and verifying the map according to the specified requirements.
+
 #include "Map.h"
 #include "MapLoader.h"
+#include <vector>
 
-void testLoadMaps() {
-    // Example code: adapt it for multiple files, handle memory cleanup etc.
-    MapLoader loader("path_to_map_file.map");
-    Map *map = loader.loadMap();
+void testLoadMaps(const std::vector<std::string>& mapFiles) {
+    for (const auto& mapFile : mapFiles) {
+        MapLoader loader(mapFile);
+        Map *map = loader.loadMap();
 
-    if(map && map->validate()) {
-        std::cout << "Valid map!" << std::endl;
-    } else {
-        std::cout << "Invalid map!" << std::endl;
+        if(map) {
+            if (map->validate()) {
+                std::cout << "Valid map loaded from: " << mapFile << std::endl;
+
+                // Demonstrating verifications:
+                // Assuming there are methods in Map class named `isConnectedGraph` and `isTerritoryInOneContinent`
+                if (map->isConnectedGraph()) {
+                    std::cout << "The map is a connected graph." << std::endl;
+                } else {
+                    std::cout << "The map is not a connected graph." << std::endl;
+                }
+
+                bool allContinentsConnected = true;
+                for (const auto& continent : map->getContinents()) {
+                    if (!map->isContinentConnected(continent)) {
+                        allContinentsConnected = false;
+                        std::cout << "Continent " << *(continent->getName()) << " is not a connected subgraph." << std::endl;
+                    }
+                }
+                if (allContinentsConnected) {
+                    std::cout << "All continents are connected subgraphs." << std::endl;
+                }
+
+                // Assuming `isTerritoryInOneContinent` checks if each territory belongs to one and only one continent
+                if (map->isTerritoryInOneContinent()) {
+                    std::cout << "Each territory belongs to one and only one continent." << std::endl;
+                } else {
+                    std::cout << "There are territories belonging to more than one continent." << std::endl;
+                }
+
+            } else {
+                std::cout << "Invalid map loaded from: " << mapFile << std::endl;
+            }
+
+            delete map;  // Clean up memory
+
+        } else {
+            std::cout << "Could not load map from: " << mapFile << std::endl;
+        }
     }
-
-    delete map;  // Clean up memory
 }
 
 int main() {
-    testLoadMaps();
+    std::vector<std::string> mapFiles = {
+        "path_to_map_file1.map",
+        "path_to_map_file2.map",
+        // ... add more map file paths
+    };
+    testLoadMaps(mapFiles);
     return 0;
 }
-
 
