38ed09b1f75f2db21b5c748a82332937c9b25685
Rue <viholrutu2000@gmail.com>
2023-10-06 15:43:32 -0400

    Update Map.h

--- Map/Map.h
+++ Map/Map.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Continent.h"
 #include "Territory.h"
+#include <unordered_set>
 
 class Map {
 public:
@@ -15,10 +16,13 @@ public:
     bool validate() const;
     void addTerritory(Territory *territory);
     void addContinent(Continent *continent);
+    bool isContinentConnected(Continent *continent) const;  // new method to check connectivity of the continents to ensure the continents are connected subgraphs
 
 private:
+    void dfs(Territory* territory, std::unordered_set<Territory*>& visited) const;  // new method
     std::vector<Territory*> territories;
     std::vector<Continent*> continents;
 };
+
 
 
