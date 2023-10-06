b5807167ab849b6952f49bc601e1af9d8128312a
Rue <viholrutu2000@gmail.com>
2023-10-06 17:06:39 -0400

    Update Continent.cpp

--- Continent/Continent.cpp
+++ Continent/Continent.cpp
@@ -1,4 +1,23 @@
 #include "Continent.h"
-
 // Implementations for Continent methods and operators go here. 
-// Remember to manage memory appropriately.
+
+Continent::Continent(const std::string &name) {
+    this->name = new std::string(name);
+}
+
+Continent::~Continent() {
+    delete name;
+}
+
+Continent::Continent(const Continent &c) {
+    name = new std::string(*(c.name));
+    territories = c.territories;
+}
+
+Continent& Continent::operator=(const Continent &c) {
+    if (this == &c) return *this;  // handle self assignment
+    delete name;  // delete the existing name
+    name = new std::string(*(c.name));
+    territories = c.territories;
+    return *this;
+}
