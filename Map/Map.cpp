cb0640901ecf880879aff66e4ea8e6c5231e0faf
Rue <viholrutu2000@gmail.com>
2023-10-06 15:44:44 -0400

    Update Map.cpp

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -1,7 +1,32 @@
+
 #include "Map.h"
 
 // Implementations for Map methods and operators go here.
 // Include validation methods, memory management etc.
+
+bool Map::isContinentConnected(Continent *continent) const {
+    if (continent->getTerritories().empty()) return false;
+
+    std::unordered_set<Territory*> visited;
+    dfs(continent->getTerritories()[0], visited);  // start DFS from the first territory
+
+    // Check if all territories in the continent were visited
+    for (auto territory : continent->getTerritories()) {
+        if (visited.find(territory) == visited.end()) return false;
+    }
+
+    return true;
+}
+
+void Map::dfs(Territory* territory, std::unordered_set<Territory*>& visited) const {
+    visited.insert(territory);
+    for (auto adjacent : territory->getAdjacencies()) {
+        if (visited.find(adjacent) == visited.end()) {
+            dfs(adjacent, visited);
+        }
+    }
+}
+
 
 /*
 
