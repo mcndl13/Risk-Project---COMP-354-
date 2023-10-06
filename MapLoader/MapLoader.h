94b6fab9b3df2b6f0d3c70d8f94505f9c0074cce
Rue <viholrutu2000@gmail.com>
2023-10-06 15:54:23 -0400

    Update MapLoader.h

--- MapLoader/MapLoader.h
+++ MapLoader/MapLoader.h
@@ -1,11 +1,15 @@
 #include <string>
 #include "Map.h"
+#include <unordered_set>
 
 class MapLoader {
 public:
     MapLoader(const std::string &filePath);
+    ~MapLoader();
     Map* loadMap();
 
 private:
     std::string *filePath;
+    std::unordered_set<std::string> assignedTerritories;  // New data member to track assigned territories
 };
+
