7cfe97f0e3c862c94a845cb0e9ff2466f6a490aa
Salmadss <146287467+Salmadss@users.noreply.github.com>
2023-10-05 20:53:11 -0400

    Update Player.h

--- Player/Player.h
+++ Player/Player.h
@@ -6,19 +6,34 @@ using namespace std;
 
 class Territory;
 
-class Player{
+class Player: public Continent; public Territory{
 
-    private:
-        vector<Territory*> owned_territories;
-        int player_ID;
-
-
-    
-    public:
         Player();
+        ~Player();
+        
+        Player(const Player &p); // copy constructor
+        Player& operator=(const Player &p); // assignment operator
+        friend std::ostream& operator<<(std::ostream &os, const Player &p); // stream insertion operator
+        
+        Continent toDefend(){
+            return owned_territories;
+        }
 
-        Player(int id);
+        toAttack(continent){
+            return continent;
+        }
 
+        issueOrder(order){
+            return order;
+        }
+
+
+ private:
+     vector<Territory*> owned_territories;
+        int player_ID;
+    Continent continent;
+    Order order;   
+ 
 };
 
 
