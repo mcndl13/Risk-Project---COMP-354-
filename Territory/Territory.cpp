fe8e644c0caf31d6788c54d740a9dc26488bb863
Rue <viholrutu2000@gmail.com>
2023-10-06 17:08:06 -0400

    Update Territory.cpp

--- Territory/Territory.cpp
+++ Territory/Territory.cpp
@@ -1,4 +1,30 @@
 #include "Territory.h"
-
 // Implementations for Territory methods and operators go here. 
-// This includes memory management: proper allocation in constructors and deallocation in destructors.
+Territory::Territory(const std::string &name) {
+    this->name = new std::string(name);
+    owningPlayer = nullptr;
+    numArmies = new int(0);
+}
+
+Territory::~Territory() {
+    delete name;
+    delete numArmies;
+}
+
+Territory::Territory(const Territory &t) {
+    name = new std::string(*(t.name));
+    owningPlayer = t.owningPlayer;  
+    numArmies = new int(*(t.numArmies));
+    adjacencies = t.adjacencies;
+}
+
+Territory& Territory::operator=(const Territory &t) {
+    if (this == &t) return *this;  // handle self assignment
+    delete name;  // delete the existing name
+    delete numArmies;  // delete the existing numArmies
+    name = new std::string(*(t.name));
+    owningPlayer = t.owningPlayer; 
+    numArmies = new int(*(t.numArmies));
+    adjacencies = t.adjacencies;
+    return *this;
+}
