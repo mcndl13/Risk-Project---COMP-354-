862d14b1b5b976aab2f36b458de298fb4229c43e
Rue <viholrutu2000@gmail.com>
2023-10-06 17:05:35 -0400

    Update Map.cpp

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -32,30 +32,3 @@ const std::vector<Continent*>& Map::getContinents() const {  // Implementation o
 
 
 
-/*
-
-map rules:
-- each territoy has a colour to represent the play
-- Gray territory is neutral
-
-
-Playing:
-- Each turn you receive new armies.  Right now, you are receiving five armies per turn.
-- The first step of every turn is deploying your new armies.
-
-
-Format:
-- Map is connected graph
-    - Node = Territory
-
-- Continent is connected subgraphs
-
-continents > territories
-
-
-1. The map must be implemented as a connected graph, where each node represents 
-   a territory.
-
-
-
-*/
