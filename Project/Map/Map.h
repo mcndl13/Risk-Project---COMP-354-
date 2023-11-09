58573401de225cb0ec122ad9443daa584ae3bcf5
Mouhamed Kairson Coundoul <107505951+mcndl13@users.noreply.github.com>
2023-11-09 08:12:44 -0500

    Merge Map.h and MapLoader.h

--- Project/Map/Map.h
+++ Project/Map/Map.h
@@ -34,6 +34,20 @@ class Map {
 
 
 
+class MapLoader {
+public:
+    MapLoader(const std::string &filePath);
+    ~MapLoader();
+    Map* loadMap();
+
+private:
+    std::string *filePath;
+    std::unordered_set<std::string> assignedTerritories;  // New data member to track assigned territories
+};
+
+
+
+
 // Continents
 class Continent {
 
