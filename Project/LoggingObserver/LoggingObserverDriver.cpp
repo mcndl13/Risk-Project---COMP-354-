b9bc4d4006233617906cd8a58b1bb3564fa224f8
Mouhamed Kairson Coundoul <107505951+mcndl13@users.noreply.github.com>
2023-11-12 22:32:23 -0500

    Add files via upload

---
+++ Project/LoggingObserver/LoggingObserverDriver.cpp
@@ -0,0 +1,49 @@
+// LoggingObserverDriver.cpp
+#include "LoggingObserver.h"
+#include "../CommandProcessing/CommandProcessing.h" // Include the header for CommandProcessor
+#include "../Orders/Orders.h"           // Include the header for Order
+#include "../GameEngine/GameEngine.h"      // Include the header for GameEngine
+
+void testLoggingObserver() {
+    // Create a LogObserver instance that will write to gamelog.txt
+    LogObserver* logObserver = new LogObserver("gamelog.txt");
+
+    // Create instances of game classes that will act as subjects
+    CommandProcessor* commandProcessor;
+    OrdersList* orderList;
+    GameEngine* gameEngine;
+
+    // Attach the observer to subjects
+    commandProcessor->attach(&logObserver);
+    orderList->attach(&logObserver);
+    gameEngine->attach(&logObserver);
+
+    // Simulate actions that trigger logging
+    // Note: These actions are illustrative and depend on the actual implementation details of your classes
+
+    // Simulate a command being processed and its effect being saved
+    Command* command = new Command(commandType::loadmap); // Assuming constructor takes a command type or description
+    commandProcessor->saveCommand(command);
+    command->saveEffect("Map loaded successfully"); // Assuming saveEffect is a member function of Command
+
+    // Simulate adding and executing an order
+    Orders* deployOrder = new DeployOrder(); // Assuming DeployOrder is a subclass of Order
+    orderList->addOrder(deployOrder);
+    deployOrder->execute(); // Assuming execute is a member function of Order that changes its state
+
+    // Simulate a game engine state transition
+    gameEngine->transition("start"); // Assuming transition is a method that changes the game engine's state
+
+    // Detach the observer before destruction or end of its scope to avoid use-after-free errors
+    commandProcessor->detach(&logObserver);
+    orderList->detach(&logObserver);
+    gameEngine->detach(&logObserver);
+
+    // Cleanup dynamically allocated memory to prevent memory leaks
+    delete deployOrder;
+}
+
+int main() {
+    testLoggingObserver();
+    return 0;
+}
