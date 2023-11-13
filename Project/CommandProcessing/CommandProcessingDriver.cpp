8e25cf7e6238b3fb91afe4d57aa626ea738be828
Niko Edjibadze <niko.edji@hotmail.com>
2023-11-12 22:38:40 -0500

    Part1Niko

--- Project/CommandProcessing/CommandProcessingDriver.cpp
+++ Project/CommandProcessing/CommandProcessingDriver.cpp
@@ -6,7 +6,15 @@
 // (2) commands can be read from a saved text file using the FileCommandProcessorAdapter class
 // (3) commands that are invalid in the current game state are rejected, and valid commands result in the correct effect and state change.
 void testCommandProcessor(){
+    GameEngine engine();
 
+    CommandProcessor* command_processor = new CommandProcessor();
+
+    Command* commnand_validatemap = new Command(commandType::validatemap);
+    Command* commnand_addplayer = new Command(commandType::addplayer);
+    Command* commnand_gamestart = new Command(commandType::gamestart);
+    Command* commnand_replay = new Command(commandType::replay);
+    Command* commnand_quit = new Command(commandType::quit);
 }
 
 
