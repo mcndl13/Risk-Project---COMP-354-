b27f0d75cfe09936c28690efe7d26e78e553a340
Rue <viholrutu2000@gmail.com>
2023-11-30 10:33:09 -0500

    testTournament function in TournamentDriver.cpp

---
+++ Project/TournamentDriver.cpp
@@ -0,0 +1,32 @@
+#include "CommandProcessing.h"
+#include "GameEngine.h"
+#include <iostream>
+
+void testTournament() {
+    CommandProcessor commandProcessor;
+    GameEngine gameEngine;
+
+    std::string tournamentCommand = "tournament -M Map1,Map2 -P Aggressive,Benevolent -G 3 -D 20";
+
+    if (commandProcessor.processTournamentCommand(tournamentCommand)) {
+        // Extract parameters from the command
+        std::vector<std::string> maps = {"Map1", "Map2"};
+        std::vector<std::string> strategies = {"Aggressive", "Benevolent"};
+        int numberOfGames = 3;
+        int maxTurns = 20;
+
+        // Execute the tournament
+        gameEngine.executeTournament(maps, strategies, numberOfGames, maxTurns);
+
+        // Output the tournament results
+        // Assuming the results are stored and accessible
+        // gameEngine.outputTournamentResults();
+    } else {
+        std::cout << "Invalid tournament command." << std::endl;
+    }
+}
+
+int main() {
+    testTournament();
+    return 0;
+}
