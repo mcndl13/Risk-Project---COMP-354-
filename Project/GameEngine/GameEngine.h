61e7834153a8de6dc88da94820bb0939ff1a7f09
Mouhamed Kairson Coundoul <107505951+mcndl13@users.noreply.github.com>
2023-12-01 11:25:07 -0500

    Correcting Syntax errors

--- Project/GameEngine/GameEngine.h
+++ Project/GameEngine/GameEngine.h
@@ -71,10 +71,7 @@ class GameEngine {
         void setMap(Map* map);
 
         // Method to execute the tournament- for A3
-        void executeTournament(const std::vector<std::string>& maps, 
-                           const std::vector<std::string>& strategies, 
-                           int numberOfGames, int maxTurns);
-};
+        void executeTournament(const std::vector<std::string>& maps, const std::vector<std::string>& strategies,int numberOfGames, int maxTurns);
 
     private:
 
